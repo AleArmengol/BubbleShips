cf99bbe575287b3b4efd9d1253b22e6f4e5aee5d
Alexis Armengol <aearmengol@gmail.com>
2019-06-04 19:46:42 -0300

    Boton disparar agregado sin implementar el listener

--- src/gui/VentanaJuego.java
+++ src/gui/VentanaJuego.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Container;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -13,12 +14,13 @@ public class VentanaJuego extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	private JLabel canon;
-	private Controlador c;
+	private JButton btnDisparar;
+	private Controlador cont;
 	
 	public VentanaJuego() {
 		configurar();
 		eventos();
-		c = new Controlador();
+		cont = new Controlador();
 		this.setTitle("Bubble Ships");
 		this.setSize(800,600);
 		this.setVisible(true);
@@ -39,7 +41,12 @@ public class VentanaJuego extends JFrame {
 		//cañon
 		canon = new JLabel();
 		canon.setIcon(new ImageIcon("imagenes\\canonSF.png"));
-		canon.setBounds(338, 468, 112, 112);
+		canon.setBounds(338, 458, 112, 112);
 		c.add(canon);
+		
+		//boton
+		btnDisparar = new JButton("Disparar");
+		btnDisparar.setBounds(450, 500, 150, 50);
+		c.add(btnDisparar);
 	}
 }
