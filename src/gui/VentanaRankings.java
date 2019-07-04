c512f24aad7a7e28d627ce240687f46b9459f469
Alexis Armengol <aearmengol@gmail.com>
2019-07-03 21:53:41 -0300

    Ordenamiento de tablas hecho, juego casi terminado, falta solo eliminar la mira

--- src/gui/VentanaRankings.java
+++ src/gui/VentanaRankings.java
@@ -3,6 +3,8 @@ package gui;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -28,28 +30,30 @@ public class VentanaRankings extends JFrame {
 		//lectura del archivo
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader("D:\\Documentos\\GitHub\\BubbleShips\\Ranking.txt"));
+			br = new BufferedReader(new FileReader("C:\\Users\\aearm\\Documents\\GitHub\\BubbleShips\\Ranking.txt"));
 			String linea;
 			
-			int cont = 0;
+			ArrayList<MapaPuntos> registrosMapeados = new ArrayList<>();
 			while((linea = br.readLine()) != null){
 				String[] registro = linea.split("\\s+");
 				String nombreRegistro = registro[0];
 				String puntRegistro = registro[1];
-				int puntRegistroInt = Integer.parseInt(puntRegistro);
-				for(int i = 0; i < 50; i++) {
-					int aux;
-					int puntoComparar = Integer.parseInt(jugadores[i][1]);
-					if(puntRegistroInt < puntoComparar){
-						for()
-					}
-				}
 				
+				Integer puntajeNuevo = Integer.parseInt(puntRegistro);
 				
-				jugadores[cont][0] = nombreRegistro;
-				jugadores[cont][1] = puntRegistro;
+				MapaPuntos mapa = new MapaPuntos(nombreRegistro, puntajeNuevo);
+				registrosMapeados.add(mapa);
+
+			}
+			Collections.sort(registrosMapeados);
+			Collections.reverse(registrosMapeados);
+			int cont = 0;
+			for(MapaPuntos actual: registrosMapeados) {
+				jugadores[cont][0] = actual.getNombre();
+				jugadores[cont][1] = actual.getPuntos().toString();
 				cont++;
 			}
+			
 		} catch (IOException e) {
 			
 		} finally {
@@ -65,6 +69,45 @@ public class VentanaRankings extends JFrame {
 		this.add(tabla);
 		this.setSize(500, 500);
 		this.setVisible(true);
+		
+	}
+	class MapaPuntos implements Comparable<MapaPuntos>{
+		
+		String nombre;
+		Integer puntos;
+		
+		
+		MapaPuntos(String nombre, Integer puntos){
+			this.nombre = nombre;
+			this.puntos = puntos;
+		}
+		@Override
+		public int compareTo(MapaPuntos arg0) {
+			Integer temp = this.getPuntos();
+			Integer temp2 = arg0.getPuntos();
+			int respuesta = temp.compareTo(temp2);
+			
+			if(respuesta == 0) {
+				String nombreAux = this.getNombre();
+				String nombreAux2 = arg0.getNombre();
+				respuesta = nombreAux2.compareTo(nombreAux);
+			}
+			//-1,0,1 menor que, igual, mayor que
+			return respuesta;
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public Integer getPuntos() {
+			return puntos;
+		}
+		public void setPuntos(Integer puntos) {
+			this.puntos = puntos;
+		}
 		
 	}
 }
